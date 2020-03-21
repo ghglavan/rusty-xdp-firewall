3a45269cf45c57f0a1e232d0a55eb4d31c11f4ce
gglavan <george.glavan27@gmail.com>
2020-03-21 15:35:36 +0200

    test

--- libbpf-helpers/src/lib.rs
+++ libbpf-helpers/src/lib.rs
@@ -97,7 +97,7 @@ mod tests {
     }
 
     #[test]
-    fn test_map_ops() {
+    fn test_hash_map_ops() {
         let m = crate::map::BpfMapCreator::<u32, u32>::new()
             .with_name("m")
             .with_type(crate::raw_libbpf::BPF_MAP_TYPE_HASH)
@@ -134,11 +134,8 @@ mod tests {
         let l = m.lookup_elem_flags(&k, crate::raw_libbpf::BPF_NOEXIST);
         assert!(l.is_err());
 
-        // not supported in 5.0 (the version that im testing with right now)
-        // let k = 1;
-        // let l = m.lookup_and_delete_elem(&k);
-
-        //BPF_F_LOCK not supported in 5.0
+        //lookup_and_delete_elem is not worky with hash maps
+        //BPF_F_LOCK is not worky with hash maps
 
         let l = m.delete_elem(&k);
         assert!(l.is_ok());
@@ -147,5 +144,29 @@ mod tests {
         let mut v = m.keys(unused_key).collect::<Vec<u32>>();
         v.sort();
         assert!(v == vec![1, 10]);
+    }
+
+    #[test]
+    fn test_array_map_ops() {
+        let m = crate::map::BpfMapCreator::<u32, u32>::new()
+            .with_name("array_m")
+            .with_type(crate::raw_libbpf::BPF_MAP_TYPE_ARRAY)
+            .with_max_entries(100)
+            .create();
+
+        assert!(m.is_ok());
+
+        let m = m.unwrap();
+        let mut k = 10_u32;
+        let mut v = 3_u32;
+
+        assert!(m.update_elem(&k, &v, crate::raw_libbpf::BPF_ANY).is_ok());
+
+        let l = m.update_elem(&k, &k, crate::raw_libbpf::BPF_F_LOCK);
+
+        if l.is_err() {
+            println!("error: {}", l.err().unwrap());
+            assert!(false);
+        }
     }
 }
